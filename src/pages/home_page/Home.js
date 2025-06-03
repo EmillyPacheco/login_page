76ea9c3b17db8002cf8f32f60e0900a10b63f58f
Emilly Pacheco <112480697+EmillyPacheco@users.noreply.github.com>
2025-06-02 21:51:38 -0300

    renderização por escalas

---
+++ src/pages/home_page/Home.js
@@ -0,0 +1,42 @@
+import { FixedSizeList } from "react-window";
+import styles from './home.module.scss';
+
+function Home({ alturaCartao = 120 }) {
+
+    const cartoes = Array.from({ length: 1000 }, (_, i) => ({
+        id: i + 1,
+        nome: `Cartao ${i + 1}`,
+    }))
+;
+
+const Row = ({ index, style }) => {
+    const cartao = cartoes[index];
+    return (
+        <div style={{ ...style, padding: "10px", boxSizing: "border-box" }}>
+            <div
+                className={styles.Card}
+            >
+                <h4> {cartao.nome} </h4>
+                <p>ID: {cartao.id} </p>
+            </div>
+        </div>
+    )
+}
+
+return<>
+<FixedSizeList 
+height={600}
+itemCount={cartoes.length}
+itemSize={alturaCartao}
+width="100vw"
+>
+{Row}
+</FixedSizeList>
+</>
+
+}
+
+export default Home;
+
+
+
